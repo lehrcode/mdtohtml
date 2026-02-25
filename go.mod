d6503f0d3f26aafce90b25cc0297651a20f17207
Christian Winkler <christian.winkler@cwkr.de>
2026-02-25 14:09:45 +0100

    table-of-contents generation

--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/lehrcode/mdtohtml
 go 1.24
 
 require (
-	github.com/alecthomas/chroma/v2 v2.20.0
+	github.com/alecthomas/chroma/v2 v2.23.1
 	github.com/alecthomas/kingpin/v2 v2.4.0
-	github.com/yuin/goldmark v1.7.13
+	github.com/yuin/goldmark v1.7.16
 	github.com/yuin/goldmark-highlighting/v2 v2.0.0-20230729083705-37449abec8cc
+	go.abhg.dev/goldmark/toc v0.12.0
 )
 
 require (
-	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/alecthomas/units v0.0.0-20240927000941-0f3dac36c52b // indirect
 	github.com/dlclark/regexp2 v1.11.5 // indirect
 	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"bytes"
 	_ "embed"
+	"html/template"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
 	chromahtml "github.com/alecthomas/chroma/v2/formatters/html"
 	"github.com/alecthomas/kingpin/v2"
 	"github.com/yuin/goldmark"
@@ -10,37 +16,43 @@ import (
 	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer/html"
-	"html/template"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
+	"go.abhg.dev/goldmark/toc"
 )
 
 type Document struct {
-	Title          string
-	ExtraVariables map[string]string
-	Stylesheets    []string
-	Body           template.HTML
+	Title         string
+	Variables     map[string]string
+	Stylesheets   []string
+	ScriptModules []string
+	Body          template.HTML
 }
 
 var (
 	inputFiles     []string
 	outputFilename string
-	document       = Document{ExtraVariables: make(map[string]string)}
+	document       = Document{Variables: make(map[string]string)}
 
 	//go:embed template.gohtml
 	outputTemplateSource   string
 	outputTemplateFilename string
 	outputTemplate         *template.Template
+	generateTOC            bool
+	titleOfTOC             string
+	minDepthOfTOC          int
+	maxDepthOfTOC          int
 )
 
 func main() {
-	kingpin.Flag("variable", "extra template variable").Short('e').StringMapVar(&document.ExtraVariables)
+	kingpin.Flag("variable", "extra template variable").Short('e').StringMapVar(&document.Variables)
 	kingpin.Flag("stylesheet", "stylesheet uri").Short('s').StringsVar(&document.Stylesheets)
+	kingpin.Flag("script-module", "JavaScript module uri").Short('j').StringsVar(&document.ScriptModules)
 	kingpin.Flag("output", "output filename").Short('o').StringVar(&outputFilename)
 	kingpin.Flag("title", "document title").Short('t').StringVar(&document.Title)
 	kingpin.Flag("template", "html template").Short('m').StringVar(&outputTemplateSource)
+	kingpin.Flag("toc", "generate table-of-contents").BoolVar(&generateTOC)
+	kingpin.Flag("toc-title", "table-of-contents title").Default("Contents").StringVar(&titleOfTOC)
+	kingpin.Flag("toc-min-depth", "minimum headline depth included in table-of-contents").IntVar(&minDepthOfTOC)
+	kingpin.Flag("toc-max-depth", "maximum headline depth included in table-of-contents").IntVar(&maxDepthOfTOC)
 	kingpin.Arg("md", "markdown filename").Required().ExistingFilesVar(&inputFiles)
 	kingpin.Parse()
 
@@ -55,14 +67,29 @@ func main() {
 		outputTemplate = template.Must(template.New("template.gohtml").Parse(outputTemplateSource))
 	}
 
+	var extensions = []goldmark.Extender{
+		extension.GFM,
+		highlighting.NewHighlighting(
+			highlighting.WithFormatOptions(
+				chromahtml.WithClasses(true),
+			),
+		),
+	}
+
+	if generateTOC {
+		extensions = append(extensions, &toc.Extender{
+			Title:    titleOfTOC,
+			TitleID:  "toc-title",
+			ListID:   "toc-list",
+			Compact:  true,
+			MinDepth: minDepthOfTOC,
+			MaxDepth: maxDepthOfTOC,
+		})
+	}
+
 	var md = goldmark.New(
 		goldmark.WithExtensions(
-			extension.GFM,
-			highlighting.NewHighlighting(
-				highlighting.WithFormatOptions(
-					chromahtml.WithClasses(true),
-				),
-			),
+			extensions...,
 		),
 		goldmark.WithParserOptions(
 			parser.WithAutoHeadingID(),
